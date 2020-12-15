34471cc2bb64e064aac5e2dd58d00ab35f0a40ac
Muneeb <43923614+muneebwanee@users.noreply.github.com>
2020-12-15 19:47:56 +0530

    Add files via upload

---
+++ app/src/main/kotlin/com/github/muneebwanee/dash/di/PerService.kt
@@ -0,0 +1,10 @@
+package com.github.muneebwanee.dash.di
+
+import javax.inject.Scope
+
+/**
+ * Created by muneebwanee on 15/12/20.
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PerService
