c7c9e5c475f64ec4dee96059bf2e0e60abb1a058
Muneeb <43923614+muneebwanee@users.noreply.github.com>
2020-12-15 20:48:37 +0530

    Add files via upload

---
+++ app/src/main/kotlin/com/github/muneebwanee/dash/ui/widget/pinlockview/CustomizationOptionsBundle.kt
@@ -0,0 +1,7 @@
+package com.github.muneebwanee.dash.ui.widget.pinlockview
+
+/**
+ * Created by muneebwanee on 15/12/20.
+ */
+data class CustomizationOptionsBundle (val textSize: Int, val buttonSize: Int, val deleteButtonSize: Int)
+
---
+++ app/src/main/kotlin/com/github/muneebwanee/dash/ui/widget/pinlockview/PinLockListener.kt
@@ -0,0 +1,8 @@
+package com.github.muneebwanee.dash.ui.widget.pinlockview
+
+/**
+ * Created by muneebwanee on 15/12/20.
+ */
+interface PinLockListener {
+    fun onComplete(pin: String)
+}
