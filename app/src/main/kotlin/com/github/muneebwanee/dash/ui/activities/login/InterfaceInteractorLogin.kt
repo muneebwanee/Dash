d1e9510535dad86d15e0944822bfd9d67e4df684
Muneeb <43923614+muneebwanee@users.noreply.github.com>
2020-12-15 20:05:55 +0530

    Add files via upload

---
+++ app/src/main/kotlin/com/github/muneebwanee/dash/ui/activities/login/InterfaceInteractorLogin.kt
@@ -0,0 +1,12 @@
+package com.github.muneebwanee.dash.ui.activities.login
+
+import com.github.muneebwanee.dash.di.PerActivity
+import com.github.muneebwanee.dash.ui.activities.base.InterfaceInteractor
+
+/**
+ * Created by muneebwanee on 15/12/20.
+ */
+@PerActivity
+interface InterfaceInteractorLogin<V : InterfaceViewLogin> : InterfaceInteractor<V> {
+    fun signInDisposable(email: String, pass: String)
+}
---
+++ app/src/main/kotlin/com/github/muneebwanee/dash/ui/activities/login/InterfaceViewLogin.kt
@@ -0,0 +1,8 @@
+package com.github.muneebwanee.dash.ui.activities.login
+
+import com.github.muneebwanee.dash.ui.activities.base.InterfaceView
+
+/**
+ * Created by muneebwanee on 15/12/20.
+ */
+interface InterfaceViewLogin : InterfaceView
