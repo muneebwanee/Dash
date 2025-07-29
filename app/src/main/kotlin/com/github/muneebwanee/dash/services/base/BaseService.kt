62cac81dd067bc61bebae8751d3c12e05bfeb5dc
Muneeb <muneebwanee@gmail.com>
2025-07-29 16:05:54 +0530

    Update BaseService.kt

--- app/src/main/kotlin/com/github/muneebwanee/dash/services/base/BaseService.kt
+++ app/src/main/kotlin/com/github/muneebwanee/dash/services/base/BaseService.kt
@@ -1,46 +1,47 @@
-package com.github.muneebwanee.dash.services.base
-
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
-import com.github.muneebwanee.dash.app.Dash
-import com.github.muneebwanee.dash.di.component.DaggerServiceComponent
-import com.github.muneebwanee.dash.di.component.ServiceComponent
-import com.github.muneebwanee.dash.di.module.ServiceModule
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-
-/**
- * Created by muneebwanee on 15/12/20.
- */
-abstract class BaseService : Service(), InterfaceService {
-
-
-    private lateinit var compositeDisposable: CompositeDisposable
-
-    companion object {
-        @JvmStatic
-        lateinit var serviceComponent: ServiceComponent
-    }
-
-    override fun onBind(p0: Intent?): IBinder? = null
-
-    override fun onCreate() {
-        super.onCreate()
-        serviceComponent = DaggerServiceComponent.builder()
-                .serviceModule(ServiceModule(this))
-                .appComponent(Dash.appComponent).build()
-        compositeDisposable = CompositeDisposable()
-    }
-
-    override fun getComponent(): ServiceComponent? = serviceComponent
-
-
-    override fun addDisposable(disposable: Disposable) {
-        compositeDisposable.add(disposable)
-    }
-
-    override fun clearDisposable() = compositeDisposable.clear()
-
-
+package com.github.muneebwanee.dash.services.base  
+  
+import android.app.Service  
+import android.content.Intent  
+import android.os.IBinder  
+import androidx.lifecycle.LifecycleService  
+import dagger.hilt.android.AndroidEntryPoint  
+import kotlinx.coroutines.CoroutineScope  
+import kotlinx.coroutines.Dispatchers  
+import kotlinx.coroutines.SupervisorJob  
+import kotlinx.coroutines.cancel  
+import timber.log.Timber  
+import javax.inject.Inject  
+  
+@AndroidEntryPoint  
+abstract class BaseService : LifecycleService() {  
+  
+    protected val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)  
+  
+    @Inject  
+    lateinit var serviceInteractor: BaseServiceInteractor  
+  
+    override fun onCreate() {  
+        super.onCreate()  
+        Timber.d("${this::class.simpleName} created")  
+    }  
+  
+    override fun onBind(intent: Intent): IBinder? = null  
+  
+    override fun onDestroy() {  
+        super.onDestroy()  
+        serviceScope.cancel()  
+        Timber.d("${this::class.simpleName} destroyed")  
+    }  
+  
+    protected fun startForegroundServiceWithNotification(  
+        notificationId: Int,  
+        channelId: String,  
+        title: String,  
+        content: String  
+    ) {  
+        val notification = serviceInteractor.createServiceNotification(  
+            channelId, title, content  
+        )  
+        startForeground(notificationId, notification)  
+    }  
 }
