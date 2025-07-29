729ae5442343577af19057ba18d51d7d94306936
Muneeb <muneebwanee@gmail.com>
2025-07-29 15:48:18 +0530

    Create DatabaseModule.kt

---
+++ app/src/main/kotlin/com/github/muneebwanee/dash/di/DatabaseModule.kt
@@ -0,0 +1,15 @@
+package com.github.muneebwanee.dash.di  
+  
+import android.content.Context  
+import androidx.room.Room  
+import com.github.muneebwanee.dash.data.database.DashDatabase  
+import dagger.Module  
+import dagger.Provides  
+import dagger.hilt.InstallIn  
+import dagger.hilt.android.qualifiers.ApplicationContext  
+import dagger.hilt.  
+  
+Wiki pages you might want to explore:  
+- [System Architecture (muneebwanee/Dash)](/wiki/muneebwanee/Dash#2)  
+- [Target Device Architecture (muneebwanee/Dash)](/wiki/muneebwanee/Dash#2.1)  
+- [Firebase Integration (muneebwanee/Dash)](/wiki/muneebwanee/Dash#2.3)
